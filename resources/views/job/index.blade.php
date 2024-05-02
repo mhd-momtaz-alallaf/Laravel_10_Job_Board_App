5ccabde2d016b285f67f5ed67e99838d9592ba2c
Alallaf1 <parental.co122@gmail.com>
2024-05-02 07:41:13 +0300

    Add some JavaScript to the text-input.blade component for reset the input and submit form when pressing on the x close icon

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,17 +3,21 @@
         :links="['Jobs' => route('jobs.index')]" />
     
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET"> {{-- using GET method with the form will add the query parameters to the route --}}
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET"> {{-- using GET method with the form will add the query parameters to the route --}}
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text" /> {{-- we used name="search" not :name="search" while passing the parameters because we want laravel to conseder it as string not as an php statement --}}
+                    <x-text-input name="search" value="{{request('search')}}" 
+                        placeholder="Search for any text" form-id="filtering-form"/> {{-- we used name="search" not :name="search" while passing the parameters because we want laravel to conseder it as string not as an php statement --}}
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{request('min_salary')}}" 
+                            placeholder="From" form-id="filtering-form"/>
+
+                        <x-text-input name="max_salary" value="{{request('max_salary')}}" 
+                            placeholder="To" form-id="filtering-form"/>
                     </div>
                 </div>
                 <div>
