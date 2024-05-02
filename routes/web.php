15a8d8404059775499df4f2c119c821987b9f9ae
Alallaf1 <parental.co122@gmail.com>
2024-05-02 12:50:06 +0300

    Create AuthController to register the users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,7 @@ Route::get('', fn() => to_route('jobs.index'));
 
 Route::resource('jobs',JobController::class)
     ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login'); // because the default laravel to use login route
+Route::resource('auth', AuthController::class)
+    ->only(['create', 'store']);
