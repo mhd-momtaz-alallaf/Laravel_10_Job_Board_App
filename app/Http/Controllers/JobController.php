a76f395b1e5bbef153420cad5387a36cdea6ba34
Alallaf1 <parental.co122@gmail.com>
2024-05-02 09:34:33 +0300

    Refactor the Filtering backend logic to be a Local Query Scope in the Job model

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,27 +12,15 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::query();
+        $filters = request()->only(
+            'search',
+            'min_salary',
+            'max_salary',
+            'experience',
+            'category'
+        );
 
-        $jobs->when(request('search'), function ($query) {
-            $query->where(function ($query) { // we use function inside function to make the results of the serach query have (and) operator with the salary queries
-                $query->where('title', 'like', '%' . request('search') . '%') // to search by job title
-                    ->orWhere('description', 'like', '%' . request('search') . '%'); // or searching by job description
-            });
-        })->when(request('min_salary'), function ($query) {
-            $query->where('salary', '>=', request('min_salary'));
-
-        })->when(request('max_salary'), function ($query) {
-            $query->where('salary', '<=', request('max_salary'));
-
-        })->when(request('experience'), function ($query) {
-            $query->where('experience', request('experience'));
-
-        })->when(request('category'), function ($query) {
-            $query->where('category', request('category'));
-        });
-
-        return view('job.index', ['jobs' => $jobs->get()]);
+        return view('job.index', ['jobs' => Job::filter($filters)->get()]); // filter is the local Query Scope (scopeFilter)
     }
 
     /**
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
 {
@@ -17,5 +19,21 @@ class Job extends Model
         'Marketing'
     ];
     
-
+    public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
+    {
+        return $query->when($filters['search'] ?? null, function ($query, $search) { // $search will use the $filters['search'] value
+            $query->where(function ($query) use ($search) { // we used function inside a function to make the results of the serach query have (and) operator with the salary queries
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {// $minSalary will use the $filters['min_salary'] value
+            $query->where('salary', '>=', $minSalary);
+        })->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {// $maxSalary will use the $filters['max_salary'] value
+            $query->where('salary', '<=', $maxSalary);
+        })->when($filters['experience'] ?? null, function ($query, $experience) {// $experience will use the $filters['experience'] value
+            $query->where('experience', $experience);
+        })->when($filters['category'] ?? null, function ($query, $category) {// $category will use the $filters['category'] value
+            $query->where('category', $category);
+        });
+    }
 }
