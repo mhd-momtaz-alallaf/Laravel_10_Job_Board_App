5c355dc8a6df5eabde33bdf93dabb56cd74bae9e
Alallaf1 <parental.co122@gmail.com>
2024-05-02 22:55:39 +0300

    perform applying for the jobs logic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -38,6 +38,14 @@
             </ul>
         </nav>
 
+        @if (session('success'))
+            <div role="alert"
+                class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+                <p class="font-bold">Success!</p>
+                <p>{{ session('success') }}</p>
+            </div>
+        @endif
+
         {{ $slot }} {{-- component slot to replace it with the actual value --}}
     </body>
 </html>
