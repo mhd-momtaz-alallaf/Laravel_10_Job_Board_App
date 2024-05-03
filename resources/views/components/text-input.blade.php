d60a412e693ecc114692c03473d417fce5b447ca
Alallaf1 <parental.co122@gmail.com>
2024-05-03 21:57:37 +0300

    show validation errors to the users in the job application

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,16 +1,38 @@
 <div class="relative">
-    @if ($formRef) {{-- if the formRef (form-ref in the view files) is passed in the index form --}}
-        <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-            @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();"> {{-- $refs is an alpine way to refference the x-ref directives, the first Alpinejs satatement is to reset the input, the second one to submit the form --}}
+    @if ('textarea' != $type)
+        @if ($formRef) {{-- if the formRef (form-ref in the view files) is passed in the index form --}}
+            <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
+                @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();"> {{-- $refs is an alpine way to refference the x-ref directives, the first Alpinejs satatement is to reset the input, the second one to submit the form --}}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" {{-- to have x clouse icon --}}
+                    stroke="currentColor" class="h-4 w-4 text-slate-500">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        @endif
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" {{-- to have x clouse icon --}}
-                stroke="currentColor" class="h-4 w-4 text-slate-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
+        <input x-ref="input-{{ $name }}" type="{{ $type }}"
+            placeholder="{{ $placeholder }}"
+            name="{{ $name }}" value="{{ old($name, $value) }}" id="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+                'pr-8' => $formRef,
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ]) />
+    @else
+        <textarea id="{{ $name }}" name="{{ $name }}" 
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+                'pr-8' => $formRef,
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ])>{{ old($name, $value) }}</textarea>
     @endif
-    <input x-ref="input-{{ $name }}" type="{{ $type }}"
-        placeholder="{{ $placeholder }}"
-        name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
-        class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
-</div>
+
+    @error($name)
+        <div class="mt-1 text-xs text-red-500">
+        {{ $message }}
+        </div>
+    @enderror
+
+    </div>
