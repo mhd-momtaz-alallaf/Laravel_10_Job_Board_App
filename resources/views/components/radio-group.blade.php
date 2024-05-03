76d5cbde98497c072d8b14b0ceefa1d0de6af567
Alallaf1 <parental.co122@gmail.com>
2024-05-03 23:28:37 +0300

    create the view for adding a job by the employer

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,14 +1,16 @@
 <div>
-    <label for="{{ $filterName }}" class="mb-1 flex items-center">
-        <input type="radio" name="{{ $filterName }}" value=""
-            @checked(!request($filterName)) />
-        <span class="ml-2">All</span>
-    </label>
+    @if ($allOption) {{-- to show the (all) option just when needed (when passed to true)--}}
+        <label for="{{ $filterName }}" class="mb-1 flex items-center">
+            <input type="radio" name="{{ $filterName }}" value=""
+                @checked(!request($filterName)) />
+            <span class="ml-2">All</span>
+        </label>
+    @endif
   
     @foreach ($optionsWithLabels as $label => $option) {{-- $optionsWithLabels is a function within the RadioGroup Component class to show the label insted of actiual aoption name --}}
         <label for="{{ $filterName }}" class="mb-1 flex items-center">
             <input type="radio" name="{{ $filterName }}" value="{{ $option }}"
-            @checked($option === request($filterName)) />
+            @checked($option === ($value ?? request($filterName))) />
             <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
