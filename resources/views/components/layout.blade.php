d955dfc19557eb9c6161f9d0b84b685f934ad49e
Alallaf1 <parental.co122@gmail.com>
2024-05-03 22:15:01 +0300

    add flash error message to the layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -48,6 +48,14 @@
             </div>
         @endif
 
+        @if (session('error'))
+            <div role="alert"
+                class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+                <p class="font-bold">Error!</p>
+                <p>{{ session('error') }}</p>
+            </div>
+        @endif
+
         {{ $slot }} {{-- component slot to replace it with the actual value --}}
     </body>
 </html>
